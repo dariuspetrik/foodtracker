5ee1cf6f1d7631754d3f17ff13a87ac060f550eb
dariuspetrik <79376740+dariuspetrik@users.noreply.github.com>
2025-05-30 14:08:08 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/food-tracker/',
+  base: '/foodtracker/',
   plugins: [
     react(),
     VitePWA({
